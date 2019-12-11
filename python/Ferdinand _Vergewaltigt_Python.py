e52e7996baec7a0afd960e3d9a85c2b6371d0149
Gornoka <piupiumacher@gmail.com>
2019-12-11 02:53:52 +0100

    74546

--- python/Ferdinand _Vergewaltigt_Python.py
+++ python/Ferdinand _Vergewaltigt_Python.py
@@ -28,24 +28,23 @@ if __name__ == '__main__':
     green_list_y = []
     blue_list_x = []
     blue_list_y = []
-    UPPER_BOUNDARY = 5
-    LOWER_BOUNDARY = -5
+    UPPER_BOUNDARY = 10
+    LOWER_BOUNDARY = -10
     STEP_SIZE = 0.01
-    RADIUS = 0.5
-    for m in [0.5, 1, 2]:
-        RADIUS = m
-        for i in np.arange(LOWER_BOUNDARY, UPPER_BOUNDARY, STEP_SIZE):
-            for i2 in np.arange(LOWER_BOUNDARY, UPPER_BOUNDARY, STEP_SIZE):
-                x = compute(i, i2, RADIUS)
-                if x[2] == INSIDE_COLOR:
-                    green_list_x.append(x[0])
-                    green_list_y.append(x[1])
-                elif x[2] == OUSIDE_COLOR:
-                    blue_list_x.append(x[0])
-                    blue_list_y.append(x[1])
-        plt.scatter(green_list_x, green_list_y, c=INSIDE_COLOR)
-        print()
-        plt.scatter(blue_list_x, blue_list_y, c=OUSIDE_COLOR)
-        plt.savefig("Radius = " + str(RADIUS) + "Ferdinand_vergewaltigt_python.png")
-        plt.clf()
-        print("completed = ", RADIUS)
+    RADIUS = 2
+
+    for i in np.arange(LOWER_BOUNDARY, UPPER_BOUNDARY, STEP_SIZE):
+        for i2 in np.arange(LOWER_BOUNDARY, UPPER_BOUNDARY, STEP_SIZE):
+            x = compute(i, i2, RADIUS)
+            if x[2] == INSIDE_COLOR:
+                green_list_x.append(x[0])
+                green_list_y.append(x[1])
+            elif x[2] == OUSIDE_COLOR:
+                blue_list_x.append(x[0])
+                blue_list_y.append(x[1])
+    plt.scatter(green_list_x, green_list_y, c=INSIDE_COLOR)
+    print()
+    plt.scatter(blue_list_x, blue_list_y, c=OUSIDE_COLOR)
+    plt.savefig("Radius = " + str(RADIUS) + "Ferdinand_vergewaltigt_python.png")
+    plt.clf()
+    print("completed = ", RADIUS)
